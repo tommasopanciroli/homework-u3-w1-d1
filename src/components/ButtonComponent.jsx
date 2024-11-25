a0a86076e75bdbcd493b30bb32f5e7ce8b160211
tommaso.panciroli <tommypanci@gmail.com>
2024-11-25 13:30:57 +0100

    adds ButtonComponent

---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,10 @@
+const ButtonComponent = (props) => {
+    return (
+        <section>
+            <h3>ECCO IL BOTTONE</h3>
+            <button> {props.text} </button>
+        </section>
+    )
+}
+
+export default ButtonComponent
